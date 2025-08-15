5564169c20a90453d0a54c863bdb5755ed35e22a
alexariv <alexarivera921@gmail.com>
2025-08-15 11:46:00 -0400

    first commit

---
+++ src/main/java/olog/stdiospring/StdiospringApplication.java
@@ -0,0 +1,13 @@
+package olog.stdiospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdiospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdiospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/olog/stdiospring/StdiospringApplicationTests.java
@@ -0,0 +1,13 @@
+package olog.stdiospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdiospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
